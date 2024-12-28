8e55aec6f04dc80c5cd573bb36a47796c3d36f65
jihandong <jihandong99@gmail.com>
2024-12-28 07:53:11 -0500

    Finish quiz5
    Signed-off-by: jihandong <jihandong99@gmail.com>

--- quiz5_intfire_noise.py
+++ quiz5_intfire_noise.py
@@ -12,8 +12,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# input current
-I = 1 # nA
 
 # capacitance and leak resistance
 C = 1 # nF
@@ -22,32 +20,40 @@ R = 40 # M ohms
 # I & F implementation dV/dt = - V/RC + I/C
 # Using h = 1 ms step size, Euler method
 
-V = 0
 tstop = 200
 abs_ref = 5 # absolute refractory period 
-ref = 0 # absolute refractory period counter
-V_trace = []  # voltage trace for plotting
 V_th = 10 # spike threshold
-spiketimes = [] # list of spike times
 
-# input current
-noiseamp = 0 # amplitude of added noise
-I += noiseamp*np.random.normal(0, 1, (tstop,)) # nA; Gaussian noise
+# I: Input current, nA
+# noise: amplitude of added noise
+def simulation(I, noise=0, label='', color='r'):
+    V = 0
+    ref = 0 # absolute refractory period counter
+    V_trace = []  # voltage trace for plotting
+    spiketimes = [] # list of spike times
 
-for t in range(tstop):
-  
-   if not ref:
-       V = V - (V/(R*C)) + (I[t]/C)
-   else:
-       ref -= 1
-       V = 0.2 * V_th # reset voltage
-   
-   if V > V_th:
-       V = 50 # emit spike
-       ref = abs_ref # set refractory counter
+    # input current
+    I += noise*np.random.normal(0, 1, (tstop,)) # nA; Gaussian noise
 
-   V_trace += [V]
+    for t in range(tstop):
+    
+       if not ref:
+           V = V - (V/(R*C)) + (I[t]/C)
+       else:
+           ref -= 1
+           V = 0.2 * V_th # reset voltage
+    
+       if V > V_th:
+           V = 50 # emit spike
+           ref = abs_ref # set refractory counter
 
+       V_trace += [V]
 
-plt.plot(V_trace)
+    plt.plot(V_trace, label=label, color=color)
+
+simulation(1, 0, label='noise 0', color='b') 
+simulation(1, 1, label='noise 1', color='g') 
+simulation(1, 2, label='noise 2', color='y') 
+simulation(1, 3, label='noise 3', color='orange') 
+simulation(1, 4, label='noise 4', color='red') 
 plt.show()
