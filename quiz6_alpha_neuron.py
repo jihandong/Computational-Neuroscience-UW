7c01eab5b1eaa4c87f4a672c2b070d7850c7d56e
jihandong <jihandong99@gmail.com>
2025-01-01 08:37:46 -0500

    Answered some questions in quiz6
    Signed-off-by: jihandong <jihandong99@gmail.com>

--- quiz6_alpha_neuron.py
+++ quiz6_alpha_neuron.py
@@ -27,14 +27,20 @@ thr = 0.9 # threshold for random spikes
 spike_train = np.random.rand(tstop) > thr
 
 # alpha func synaptic conductance
-t_a = 100 # Max duration of syn conductance
-t_peak = 1 # ms
-g_peak = 0.05 # nS (peak synaptic conductance)
-const = g_peak / (t_peak*np.exp(-1));
-t_vec = np.arange(0, t_a + h, h)
-alpha_func = const * t_vec * (np.exp(-t_vec/t_peak))
+t_peaks = np.arange(0.5, 10.5, 0.5).tolist()
+colors = plt.cm.viridis(np.linspace(0, 1, len(t_peaks)))
+alpha_funcs = []
 
-plt.plot(t_vec[:80], alpha_func[:80])
+for i, t_peak in enumerate(t_peaks): # ms
+    t_a = 100 # Max duration of syn conductance
+    g_peak = 0.05 # nS (peak synaptic conductance)
+    const = g_peak / (t_peak*np.exp(-1))
+    t_vec = np.arange(0, t_a + h, h)
+    alpha_func = const * t_vec * (np.exp(-t_vec/t_peak))
+    alpha_funcs += [alpha_func]
+
+    plt.plot(t_vec[:80], alpha_func[:80], color=colors[i])
+
 plt.xlabel('t (in ms)')
 plt.title('Alpha Function (Synaptic Conductance for Spike at t=0)')
 plt.draw()
@@ -46,64 +52,73 @@ R = 40 # M ohms
 print('C = {}'.format(C))
 print('R = {}'.format(R))
 
-# conductance and associated parameters to simulate spike rate adaptation
-g_ad = 0
-G_inc = 1/h
-tau_ad = 2
+firing_counts = []
+for i, alpha_func in enumerate(alpha_funcs):
+    # conductance and associated parameters to simulate spike rate adaptation
+    g_ad = 0
+    G_inc = 1/h
+    tau_ad = 2
 
-# Initialize basic parameters
-E_leak = -60 # mV, equilibrium potential
-E_syn = 0 # Excitatory synapse (why is this excitatory?)
-g_syn = 0 # Current syn conductance
-V_th = -40 # spike threshold mV
-V_spike = 50 # spike value mV
-ref_max = 4/h # Starting value of ref period counter
-t_list = np.array([], dtype=int)
-V = E_leak
-V_trace = [V]
-t_trace = [0]
+    # Initialize basic parameters
+    E_leak = -60 # mV, equilibrium potential
+    E_syn = 0 # Excitatory synapse (why is this excitatory?)
+    g_syn = 0 # Current syn conductance
+    V_th = -40 # spike threshold mV
+    V_spike = 50 # spike value mV
+    ref_max = 4/h # Starting value of ref period counter
+    t_list = np.array([], dtype=int)
+    V = E_leak
+    V_trace = [V]
+    t_trace = [0]
+    firing_count = 0
 
-fig, axs = plt.subplots(2, 1)
-axs[0].plot(np.arange(0,t_max,h), spike_train)
-axs[0].set_title('Input spike train')
+    for t in range(tstop):
 
-for t in range(tstop):
+        # Compute input
+        if spike_train[t]: # check for input spike
+            t_list = cc([t_list, [1]])
 
-    # Compute input
-    if spike_train[t]: # check for input spike
-        t_list = cc([t_list, [1]])
+        # Calculate synaptic current due to current and past input spikes
+        g_syn = np.sum(alpha_func[t_list])
+        I_syn = g_syn*(E_syn - V) 
 
-    # Calculate synaptic current due to current and past input spikes
-    g_syn = np.sum(alpha_func[t_list])
-    I_syn = g_syn*(E_syn - V) 
+        # Update spike times
+        if np.any(t_list):
+            t_list = t_list + 1
+            if t_list[0] == t_a: # Reached max duration of syn conductance
+                t_list = t_list[1:]
 
-    # Update spike times
-    if np.any(t_list):
-        t_list = t_list + 1
-        if t_list[0] == t_a: # Reached max duration of syn conductance
-            t_list = t_list[1:]
+        # Compute membrane voltage
+        # Euler method: V(t+h) = V(t) + h*dV/dt
+        if not ref:
+            V = V + h*(-((V-E_leak)*(1+R*g_ad)/(R*C)) + (I_syn/C))
+            g_ad = g_ad + h*(-g_ad/tau_ad) # spike rate adaptation
+        else:
+            ref -= 1
+            V = V_th - 10 # reset voltage after spike
+            g_ad = 0
 
-    # Compute membrane voltage
-    # Euler method: V(t+h) = V(t) + h*dV/dt
-    if not ref:
-        V = V + h*(-((V-E_leak)*(1+R*g_ad)/(R*C)) + (I_syn/C))
-        g_ad = g_ad + h*(-g_ad/tau_ad) # spike rate adaptation
-    else:
-        ref -= 1
-        V = V_th - 10 # reset voltage after spike
-        g_ad = 0
+        # Generate spike
+        if (V > V_th) and not ref:
+            V = V_spike
+            ref = ref_max
+            g_ad = g_ad + G_inc
+            firing_count += 1
 
-    # Generate spike
-    if (V > V_th) and not ref:
-        V = V_spike
-        ref = ref_max
-        g_ad = g_ad + G_inc
+        V_trace += [V]
+        t_trace += [t*h]
 
-    V_trace += [V]
-    t_trace += [t*h]
+    if i%5 == 0:
+        fig, axs = plt.subplots(2, 1)
+        axs[0].plot(np.arange(0,t_max,h), spike_train)
+        axs[0].set_title('Input spike train')
+        axs[1].plot(t_trace, V_trace, color=colors[i])
+        axs[1].set_title('Output spike train (t_peak = {} ms)'.format(t_peaks[i]))
+        plt.draw()
+    firing_counts += [firing_count]
 
-
-axs[1].plot(t_trace,V_trace)
-plt.draw()
-axs[1].set_title('Output spike train')
+plt.figure()
+plt.plot(t_peaks, firing_counts)
+plt.xlabel('t_peak (in ms)')
+plt.ylabel('Firing Times')
 plt.show()
