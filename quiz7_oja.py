73aaa382c513a7cb4f62f4cb764cf2c44e725b6b
jihandong <jihandong99@gmail.com>
2025-01-04 04:39:14 -0500

    Finish quiz7: add data central offset and try Hebb rule
    Signed-off-by: jihandong <jihandong99@gmail.com>

--- quiz7_oja.py
+++ quiz7_oja.py
@@ -5,28 +5,32 @@ import matplotlib.pyplot as plt
 with open('c10p1.pickle', 'rb') as f:
     data = pickle.load(f)
 
+# Load the data and add a random offset
 x = data['c10p1']
+centrl = np.random.rand(2)
+print(f"centrl = {centrl}")
+x += centrl
 plt.scatter(x[:,0], x[:,1])
 plt.draw()
 
+# The parameters
 eta = 1
-alpha = 1
+alpha = 1 # zero for Hebbian learning, which would explode
 dt = 0.01
-nb_test = 16
+nb_test = 10
 nb_iter = 50000
 
+# Apply Ojia's rule to learn the principal component
 colors = plt.cm.viridis(np.linspace(0, 1, nb_test))
 for i in range(nb_test):
     trace = np.zeros((nb_iter, 2))
-    w = np.random.rand(2)
+    w = np.random.uniform(-2, 2, 2)
 
     for j in range(nb_iter):
         u = x[j % x.shape[0]]
         v = np.dot(u, w)
         w += dt * eta * (v * u - alpha * v**2 * w)
         trace[j] = w
-        #if j % 10000 == 0:
-        #    print(f'test{i}, iter{j}, w=', w)
 
     plt.scatter(trace[:-1,0], trace[:-1,1], color=colors[i])
     plt.scatter(trace[-1,0], trace[-1:,1], color='r')
@@ -50,3 +54,4 @@ eigenvector. w can converge to either of these two vectors.
 
 But, the final weights are not the same as the eigenvectors of
 the correlation matrix. why?
+"""
